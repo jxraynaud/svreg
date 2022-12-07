baba3f3dcf42e738852b272f6466599df2f125cb
Yannick Stoll <yannick.stoll@pixelforest.io>
2022-12-07 17:39:18 +0100

    compute_usefullness work, todo: compute shapley value and be carefull on side effect wrt subset.remove

--- sv_regression.py
+++ sv_regression.py
@@ -1,11 +1,18 @@
 import numpy as np
 import pandas as pd
 
+from itertools import chain, combinations
+from timeit import default_timer as timer
 from icecream import ic
+from sklearnex import patch_sklearn, config_context
+
+patch_sklearn()
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
 from scipy.stats import pearsonr
 from icecream import ic
+from typing import Tuple
 
 # path_dataset = "data/base_test_sv_reg_working.csv"
 # data_sv = pd.read_csv(path_dataset,
@@ -42,7 +49,7 @@ class SvRegression():
                                    parse_dates=True,
                                    infer_datetime_format=True)
         # Check that target is indeed in the dataset.
-        assert target in self.data_sv.columns
+        # assert target in self.data_sv.columns
 
         # Todo: find a way more subtle to handle missing values.
         n_rows = self.data_sv.shape[0]
@@ -61,11 +68,16 @@ class SvRegression():
         self._scaler_x = StandardScaler()
         self._scaler_y = StandardScaler()
 
+        # Normalizing x_features and y_target.
+        self.x_features_norm = self._scaler_x.fit_transform(self.x_features)
+        self.y_target_norm = self._scaler_y.fit_transform(self.y_target.reshape(-1, 1))
+        # print(f"max: {np.amax(self.x_features_norm)}")  # just to check normalization.
+
         # initializing C (sample correlations, size (n, n)) and r (sample-target correlations, size (n,)).
         self.c_jk = np.matmul(self.x_features.T, self.x_features)
-        assert self.c_jk.shape == (self.x_features.shape[1], self.x_features.shape[1])
+        # assert self.c_jk.shape == (self.x_features.shape[1], self.x_features.shape[1])
         self.r_j = np.matmul(self.x_features.T, self.y_target)
-        assert self.r_j.shape == (self.x_features.shape[1],)
+        # assert self.r_j.shape == (self.x_features.shape[1],)
 
     def normalize(self):
         x_features_norm = self._scaler_x.fit_transform(self.x_features)
@@ -77,13 +89,40 @@ class SvRegression():
         y_features = self._scaler_y.inverse_transform(y_features_norm)
         return x_features, y_features
 
-    def get_b(self):
-        c_inv = np.linalg.inv(self.c_jk)
-        return np.matmul(c_inv, self.r_j)
+    def get_c_jk(self):
+        return np.matmul(self.x_features_norm.T, self.x_features_norm)
 
-    def get_r_squared(self):
-        b_coeffs = self.get_b()
-        return np.inner(b_coeffs, self.r_j)
+    def get_r_j(self):
+        return np.matmul(self.x_features_norm.T, self.y_target_norm)
+
+    def get_b(self):
+        c_inv = np.linalg.inv(self.get_c_jk())
+        return np.matmul(c_inv, self.get_r_j())
+
+
+    def get_r_squared(self, predictors: Tuple[int, ...] = None) -> float:
+        n_features = self.x_features_norm.shape[1]
+        if predictors is not None:
+            if not all(0 <= elem <= n_features for elem in predictors):
+                raise ValueError(f"All elements of predictors must be in between 0 and {n_features - 1}.")
+        # Defining x_features that corresponds to the model defined by predictors:
+            x_features_norm = self.x_features_norm[:, predictors]
+        else:
+        # We get the model with all predictors.
+            x_features_norm = self.x_features_norm
+        # Targets (shape=(N, 1)) do not change in either cases.
+        y_target_norm = self.y_target_norm
+        n_features = x_features_norm.shape[1]
+        c_jk_norm = np.matmul(x_features_norm.T, x_features_norm)
+        # assert c_jk_norm.shape == (n_features, n_features)
+        r_j_norm = np.matmul(x_features_norm.T, y_target_norm)
+        # assert r_j_norm.shape == (n_features, 1)
+        b_j_norm = np.matmul(np.linalg.inv(c_jk_norm), r_j_norm)
+        # assert b_j_norm.shape == (n_features, 1)
+        r_squared_norm = np.matmul(b_j_norm.T, r_j_norm)
+
+        return r_squared_norm.ravel().item()
+
 
 # Testing:
 dataset = "data/base_test_sv_reg_working.csv"
@@ -91,6 +130,7 @@ sv_reg = SvRegression(data=dataset,
                       target="qlead_auto")
 
 x_features_norm, y_target_norm = sv_reg.normalize()
+
 x_features, y_target = sv_reg.unnormalize(x_features_norm, y_target_norm)
 print("shape: y_target not normalized:")
 print(y_target.shape)
@@ -98,10 +138,106 @@ print(y_target.shape)
 print("shape: y_target normalized:")
 print(y_target_norm.shape)
 
-b_coeffs = sv_reg.get_b()
-print("shape: b_coeffs:")
-print(b_coeffs.shape)
+# b_coeffs = sv_reg.get_b()
+# print("shape: b_coeffs:")
+# print(b_coeffs.shape)
 
-r_squared_dum = sv_reg.get_r_squared()
-print("r_squared_dum:")
-print(r_squared_dum)
+# r_squared_dum = sv_reg.get_r_squared(predictors=(5, 20, 8))
+# print("r_squared_dum:")
+# print(r_squared_dum)
+
+dum_num = 5
+dum_predictors = list(range(dum_num))
+x_features_norm_dum = x_features_norm[:, dum_predictors]
+
+# declaring model
+lin_reg = LinearRegression(n_jobs=-1, copy_X=False)
+
+
+# Computing r_squared for all 2**dum_num combinations amongst predictors.
+# r_squared_dum = [0]
+# for ind in range(1, 2**dum_num):
+#     ic(ind)
+
+# # TODO: find a way to format the format code dynamically (e.g replace '5' by dum_num at runtime).
+#     dum_str = f"{ind:05b}"
+
+#     ic(dum_str)
+#     ic(dum_str[::-1])
+#     dum_mask = [bool(int(ind_f)) for ind_f in dum_str[::-1]]
+#     x_features_curr = x_features_norm_dum[:, dum_mask]
+#     lin_reg.fit(x_features_curr, y_target_norm)
+#     r_squared_dum.append(lin_reg.score(x_features_curr, y_target_norm))
+
+def get_rsquared_sk(ind=None):
+    if ind == 0:
+        return 0.0
+    else:
+        ind_bin = f"{ind:05b}"
+        dum_mask = [bool(int(ind_f)) for ind_f in ind_bin[::-1]]
+        x_features_curr = x_features_norm_dum[:, dum_mask]
+        lin_reg.fit(x_features_curr, y_target_norm)
+        return lin_reg.score(x_features_curr, y_target_norm)
+
+# Activate GPU acceleration.
+# Problem: requires dpctl to work:
+# https://pypi.org/project/dpctl/
+
+# I had issue installing it, turning off for now.
+# with config_context(target_offload="gpu:0"):
+
+start = timer()
+r_squared_dum_compr = [get_rsquared_sk(ind) for ind in range(0, 2**dum_num)]
+time_comp = timer() - start
+
+# Comptue usefullness as defined by formulae 18 from the article.
+def compute_usefullness(predictors=None, target=2, len_predictors=4):
+
+    if len(predictors) == 1:
+
+        # Rsquared corresponding to length 1 predictors:
+        bin_predictors = [1 if x in predictors else 0 for x in range(len_predictors)]
+        ind_rsquared = int("".join(str(x) for x in bin_predictors), 2)
+        r_squared = r_squared_dum_compr[ind_rsquared]
+
+        return r_squared
+
+    else:
+        # Rsquared with target:
+        bin_predictors = [1 if x in predictors else 0 for x in range(len_predictors)]
+        ind_rsquared = int("".join(str(x) for x in bin_predictors), 2)
+        r_squared_with_target = r_squared_dum_compr[ind_rsquared]
+
+        # Rsquared without target:
+        predictors.remove(target)
+        bin_predictors_without_target = [1 if x in predictors else 0 for x in range(len_predictors)]
+        ind_rsquared_without_target = int("".join(str(x) for x in bin_predictors_without_target), 2)
+        r_squared_with_target_without_target = r_squared_dum_compr[ind_rsquared_without_target]
+
+        return r_squared_with_target - r_squared_with_target_without_target
+
+dum_us = compute_usefullness(predictors=[0, 2, 3], target=2, len_predictors=dum_num)
+
+
+# Test Case: Computing Shapley value for the second predictor.
+dum_target = 2
+ic(dum_predictors)
+
+# First, second, third etc... term
+# for len_comb in range(len(dum_predictors)):
+
+for subset in filter(lambda x: dum_target in x, combinations(dum_predictors, 3)):
+    # subset = list(subset)  # convert to list to make it mutable, need to remove target (see function below).
+    # dum_us = compute_usefullness(predictors=subset, target=dum_target, len_predictors=len(dum_predictors))
+    ic(subset)
+    #ic(dum_us)
+
+
+
+
+# ic(r_squared_dum_compr)
+# ic(dum_us)
+# ic(time_comp)
+
+
+

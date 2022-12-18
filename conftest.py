5ab024e49867c8f4cf9b22f043cc1b3480fdb6c3
Zekao <Busterk182@gmail.com>
2022-12-19 00:09:10 +0100

    Added cache gestion for the tests, added conftest & updated test.py + tox.ini

---
+++ conftest.py
@@ -0,0 +1,52 @@
+import pytest
+from sv_regression import SvRegression
+
+@pytest.fixture
+def cache_compute_1_feature(request):
+    dataset = "data/base_test_sv_reg_working.csv"
+    shapley = request.config.cache.get("shapley_1", None)
+    if shapley is None:
+        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        shapley = sv_reg.compute_shapley()
+        request.config.cache.set("shapley_1", shapley)
+    return shapley
+
+@pytest.fixture
+def cache_compute_5_features(request):
+    dataset = "data/base_test_sv_reg_working.csv"
+    shapley = request.config.cache.get("shapley_2", None)
+    if shapley is None:
+        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        shapley = sv_reg.compute_shapley()
+        request.config.cache.set("shapley_2", shapley)
+    return shapley
+
+@pytest.fixture
+def cache_compute_10_features(request):
+    dataset = "data/base_test_sv_reg_working.csv"
+    shapley = request.config.cache.get("shapley_3", None)
+    if shapley is None:
+        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        shapley = sv_reg.compute_shapley()
+        request.config.cache.set("shapley_3", shapley)
+    return shapley
+
+@pytest.fixture
+def cache_compute_15_features(request):
+    dataset = "data/base_test_sv_reg_working.csv"
+    shapley = request.config.cache.get("shapley_4", None)
+    if shapley is None:
+        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+        shapley = sv_reg.compute_shapley()
+        request.config.cache.set("shapley_4", shapley)
+    return shapley
+
+@pytest.fixture
+def cache_norm_shap(request):
+    dataset = "data/base_test_sv_reg_working.csv"
+    test_dict = request.config.cache.get("test_dict", None)
+    if test_dict is None:
+        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4])
+        test_dict = sv_reg.check_norm_shap()
+        request.config.cache.set("test_dict", test_dict)
+    return test_dict
--- test.py
+++ test.py
@@ -1,117 +1,102 @@
 from sv_regression import SvRegression, LinearRegression, StandardScaler
-from unittest import TestCase
 import pytest
 
-class TryTesting(TestCase):
-    def test_init(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto")
-        self.assertEqual(sv_reg.x_features.shape, (643, 27))
-
-    def test_init_2(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        with pytest.raises(ValueError):
-            sv_reg = SvRegression(data=dataset, target="invalid_target")
-
-    def test_normalize(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto")
-        x_features_norm, y_target_norm = sv_reg.normalize()
-        self.assertEqual(x_features_norm.shape, (643, 27)) 
-
-    def test_unnormalize(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto")
-        x_features_norm, y_target_norm = sv_reg.normalize()
-        x_features, y_target = sv_reg.unnormalize(x_features_norm, y_target_norm)
-        self.assertEqual(x_features.shape, (643, 27))   
-
-    def test_get_rsquared(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto")
-        rsquared = sv_reg._get_rsquared_sk(sv_reg.num_feat_selec)
-        self.assertEqual(round(rsquared, 3), 0.885) 
-        
-    def test_get_rsquared_2(self): 
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto")
-        rsquared = sv_reg._get_rsquared_sk(0)
-        self.assertEqual(rsquared, 0)
-
-    def test_compute_shapley_incorrect_target(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        with pytest.raises(ValueError):
-            sv_reg = SvRegression(data=dataset, target="incorrect_t", ind_predictors_selected=[0, 1, 2, 3, 4])   
-            shapley = sv_reg.compute_shapley(target_pred=6) 
-
-
-    def test_compute_shapley_1_feature(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0])
-        shapley = sv_reg.compute_shapley(ind_feat=0)
-        self.assertEqual(round(shapley, 1), 0.7)  
-
-    def test_compute_shapley_5_features(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4])
-        shapley = sv_reg.compute_shapley()
-        self.assertEqual(round(shapley, 3), 0.128)
-
-    def test_compute_shapley_10_features(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        shapley = sv_reg.compute_shapley()
-        self.assertEqual(round(shapley, 3), 0.022)
-
-    def test_compute_shapley_none(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        with pytest.raises(ValueError):
-            sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[])
-            shapley = sv_reg.compute_shapley()
-
-    def test_compute_shapley_incorrect(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        with pytest.raises(ValueError):
-            sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0])
-            sv_reg._list_r_squared = None
-            shapley = sv_reg.compute_shapley(ind_feat=5)    
-
-    def test_check_norm_shap(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4])
-        # shapley = sv_reg.compute_shapley()
-        test_dict = sv_reg.check_norm_shap()
-        # compare the values of my dict
-        self.assertEqual(test_dict['r_squared_full'], test_dict['sum_shaps'])  
-
-    def test_data_sv(self):
-        dataset = "data/base_test_sv_reg_working.csv"       
-        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4])
-        data = sv_reg.data_sv
-        self.assertEqual(data.shape, (643, 28))
-
-    def test_fit(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4])
-        sv_reg.fit()
-        self.assertEqual(sv_reg.x_features.shape, (643, 5))
-
-    def test_fit_incorrect(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        with pytest.raises(ValueError):
-            sv_reg = SvRegression(data=dataset, target="incorrect_t", ind_predictors_selected=[0, 1, 2, 3, 4, 5])
-            sv_reg.fit()
-
-    def test_fit_incorrect_2(self):
-        dataset = "data/base_test_sv_reg_working.csv"
-        with pytest.raises(ValueError):
-            sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[])
-            sv_reg.fit()
-
-
-if __name__ == '__main__':
+def test_init():
     dataset = "data/base_test_sv_reg_working.csv"
-    # sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4])
-    # shapley = sv_reg.compute_shapley()
-    # data = sv_reg.data_sv
-    # print(data.shape)
+    sv_reg = SvRegression(data=dataset, target="qlead_auto")
+    assert sv_reg.x_features.shape == (643, 27)
+
+def test_init_2():
+    dataset = "data/base_test_sv_reg_working.csv"
+    with pytest.raises(ValueError):
+        sv_reg = SvRegression(data=dataset, target="invalid_target")
+
+def test_normalize():
+    dataset = "data/base_test_sv_reg_working.csv"
+    sv_reg = SvRegression(data=dataset, target="qlead_auto")
+    x_features_norm, y_target_norm = sv_reg.normalize()
+    assert x_features_norm.shape == (643, 27)
+
+
+def test_unnormalize():
+    dataset = "data/base_test_sv_reg_working.csv"
+    sv_reg = SvRegression(data=dataset, target="qlead_auto")
+    x_features_norm, y_target_norm = sv_reg.normalize()
+    x_features, y_target = sv_reg.unnormalize(x_features_norm, y_target_norm)
+    assert x_features.shape == (643, 27)
+
+def test_get_rsquared():
+    dataset = "data/base_test_sv_reg_working.csv"
+    sv_reg = SvRegression(data=dataset, target="qlead_auto")
+    rsquared = sv_reg._get_rsquared_sk(sv_reg.num_feat_selec)
+    assert round(rsquared, 3) == 0.885
+
+def test_get_rsquared_2():
+    dataset = "data/base_test_sv_reg_working.csv"
+    sv_reg = SvRegression(data=dataset, target="qlead_auto")
+    rsquared = sv_reg._get_rsquared_sk(0)
+    assert rsquared == 0
+
+def test_compute_shapley_incorrect_target():
+    dataset = "data/base_test_sv_reg_working.csv"
+    with pytest.raises(ValueError):
+        sv_reg = SvRegression(data=dataset, target="incorrect_t", ind_predictors_selected=[0, 1, 2, 3, 4])
+        shapley = sv_reg.compute_shapley(ind_feat=6)
+
+def test_compute_shapley_1_feature(cache_compute_1_feature):
+    shapley = cache_compute_1_feature
+    assert round(shapley, 2) == 0.02
+
+def test_compute_shapley_5_features(cache_compute_5_features):
+    shapley = cache_compute_5_features
+    assert round(shapley, 3) == 0.022
+
+def test_compute_shapley_10_features(cache_compute_10_features):
+    shapley = cache_compute_10_features
+    assert round(shapley, 3) == 0.022
+
+def test_compute_shapley_15_features(cache_compute_15_features):
+    shapley = cache_compute_15_features
+    assert round(shapley, 3) == 0.132
+
+
+def test_compute_shapley_none():
+    dataset = "data/base_test_sv_reg_working.csv"
+    with pytest.raises(ValueError):
+        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[])
+        shapley = sv_reg.compute_shapley()
+
+def test_compute_shapley_incorrect():
+    dataset = "data/base_test_sv_reg_working.csv"
+    with pytest.raises(ValueError):
+        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0])
+        sv_reg._list_r_squared = None
+        shapley = sv_reg.compute_shapley(ind_feat=5)
+
+def test_check_norm_shap(cache_norm_shap):
+    test_dict = cache_norm_shap
+    assert test_dict['r_squared_full'] == test_dict['sum_shaps']
+
+def test_data_sv():
+    dataset = "data/base_test_sv_reg_working.csv"
+    sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4])
+    data = sv_reg.data_sv
+    assert data.shape == (643, 28)
+
+def test_fit():
+    dataset = "data/base_test_sv_reg_working.csv"
+    sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[0, 1, 2, 3, 4])
+    sv_reg.fit()
+    assert sv_reg.x_features.shape == (643, 5)
+
+def test_fit_incorrect():
+    dataset = "data/base_test_sv_reg_working.csv"
+    with pytest.raises(ValueError):
+        sv_reg = SvRegression(data=dataset, target="incorrect_t", ind_predictors_selected=[0, 1, 2, 3, 4, 5])
+        sv_reg.fit()
+
+def test_fit_incorrect_2():
+    dataset = "data/base_test_sv_reg_working.csv"
+    with pytest.raises(ValueError):
+        sv_reg = SvRegression(data=dataset, target="qlead_auto", ind_predictors_selected=[])
+        sv_reg.fit()
